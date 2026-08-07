efe64f0f97455ea4b25c9fac90c6dc94b5dfc32a
CAGOJ <OLIVEIRACAGOJ@GMAIL.COM>
2026-08-06 21:46:31 -0300

    Update fitlyne-config.js

--- fitlyne-config.js
+++ fitlyne-config.js
@@ -7,7 +7,7 @@
  */
 window.FITLYNE_CONFIG = Object.freeze({
   BUILD: "2026-08-06-loja-pro",
-  API_URL: "https://script.google.com/macros/s/AKfycbz8wSvAXMMhUwpvlXUcPp948mtf4gZ9-Sak5UynSyZfI9c9gsM-AeMH7Xwo4eLcNdkO-g/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxsea_d2uMckXf5zqHrHZmO5D1ZJC8ZnGCoMcVp0FQb2mSShbsuksvJePpn_i-xlVyH4Q/exec",
   CLOUDINARY_CLOUD_NAME: "v9gfcyqm",
   CLOUDINARY_UPLOAD_PRESET: "fitlyne_upload",
   CLOUDINARY_WATERMARK_PUBLIC_ID: "",
